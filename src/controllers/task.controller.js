c5e0312b88bd248886bb38c1efc85d2d1d10d4c9
Jonas Costa <jonascosta0299@gmail.com>
2024-12-04 04:53:57 -0300

    controllers das tarefas adicionados

--- src/controllers/task.controller.js
+++ src/controllers/task.controller.js
@@ -6,12 +6,83 @@ class TaskController {
         this.res = res;
     }
 
-    async getTasks() {
+    async getAll() {
         try {
             const tasks = await TaskModel.find({});
             this.res.status(200).send(tasks);
         } catch (err) {
             this.res.status(500).send(error.message);
+        }
+    }
+
+    async insert() {
+        try {
+            const newTask = new TaskModel(this.req.body);
+
+            await newTask.save();
+
+            this.res.status(201).send(newTask);
+        } catch (err) {
+            this.res.status(500).send(error.message);
+        }
+    }
+
+    async getById() {
+        try {
+            const taskId = this.req.params.id;
+            const tasks = await TaskModel.findById(taskId);
+
+            if (!tasks) {
+                return res.status(404).send("Essa tarefa não foi encontrada");
+            }
+
+            this.res.status(200).send(tasks);
+        } catch (err) {
+            this.res.status(500).send(err.message);
+        }
+    }
+
+    async update() {
+        try {
+            const taskId = this.req.params.id;
+            const taskData = this.req.body;
+
+            const taskToUpdate = await TaskModel.findById(taskId);
+
+            const allowedUpdates = ["isCompleted"];
+
+            const requestedUpdates = Object.keys(taskData);
+
+            for (let i = 0; i <= requestedUpdates.length - 1; i++) {
+                if (allowedUpdates.includes(requestedUpdates[i])) {
+                    taskToUpdate[requestedUpdates[i]] =
+                        taskData[requestedUpdates[i]];
+                }
+            }
+
+            await taskToUpdate.save();
+
+            return this.res.status(200).send(taskToUpdate);
+        } catch (err) {
+            this.res.status(500).send(err.message);
+        }
+    }
+
+    async delete() {
+        try {
+            const taskId = this.req.params.id;
+
+            const taskToDeleted = await TaskModel.findById(taskId);
+
+            if (!taskToDeleted) {
+                return this.res.status(404).send("Tarefa não encontrada");
+            }
+
+            const deletedTask = await TaskModel.findByIdAndDelete(taskId);
+
+            this.res.status(200).send(deletedTask);
+        } catch (err) {
+            this.res.status(500).send(err.message);
         }
     }
 }
--- src/routes/task.routes.js
+++ src/routes/task.routes.js
@@ -6,77 +6,23 @@ const TaskController = require("./../controllers/task.controller");
 const router = express.Router();
 
 router.get("/", async (req, res) => {
-    return new TaskController(req, res).getTasks();
+    return new TaskController(req, res).getAll();
 });
 
 router.post("/", async (req, res) => {
-    try {
-        const newTask = new TaskModel(req.body);
-
-        await newTask.save();
-
-        res.status(201).send(newTask);
-    } catch (err) {
-        res.status(500).send(error.message);
-    }
-});
-
-router.patch("/:id", async (req, res) => {
-    try {
-        const taskId = req.params.id;
-        const taskData = req.body;
-
-        const taskToUpdate = await TaskModel.findById(taskId);
-
-        const allowedUpdates = ["isCompleted"];
-
-        const requestedUpdates = Object.keys(taskData);
-
-        for (update of requestedUpdates) {
-            if (allowedUpdates.includes(update)) {
-                taskToUpdate[update] = taskData[update];
-            }
-        }
-
-        await taskToUpdate.save();
-
-        return res.status(200).send(taskToUpdate);
-    } catch (err) {
-        res.status(500).send(err.message);
-    }
-});
-
-router.delete("/:id", async (req, res) => {
-    try {
-        const taskId = req.params.id;
-
-        const taskToDeleted = await TaskModel.findById(taskId);
-
-        if (!taskToDeleted) {
-            return res.status(404).send("Tarefa não encontrada");
-        }
-
-        const deletedTask = await TaskModel.findByIdAndDelete(taskId);
-
-        res.status(200).send(deletedTask);
-    } catch (err) {
-        res.status(500).send(err.message);
-    }
+    return new TaskController(req, res).insert();
 });
 
 router.get("/:id", async (req, res) => {
-    try {
-        const taskId = req.params.id;
-        const tasks = await TaskModel.findById(taskId);
+    return new TaskController(req, res).getById();
+});
 
-        if (!tasks) {
-            return res.status(404).send("Essa tarefa não foi encontrada");
-        }
+router.patch("/:id", async (req, res) => {
+    return new TaskController(req, res).update();
+});
 
-        res.status(200).send(tasks);
-    } catch (err) {
-        res.status(500).send(err.message);
-    }
+router.delete("/:id", async (req, res) => {
+    return new TaskController(req, res).delete();
 });
 
 module.exports = router;
